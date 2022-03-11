c90bdf42e865cdfd925c0ffe22e2496426a4629b
khaledCSE <khaledcse10@gmail.com>
2022-03-11 13:48:26 +0600

    Video background fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,28 @@
 import './Hero.scss';
+import heroVideo from '../assets/hero_video.mp4';
+import heroVideoPoster from '../assets/hero-video-poster.jpg';
+import { Button, Container } from 'react-bootstrap';
 
 const Hero = () => {
-    return <div className="hero"></div>;
+    return (
+        <div className="hero">
+            <video poster={heroVideoPoster} autoPlay loop muted>
+                <source src={heroVideo} type="video/mp4" />
+                Video not supported
+            </video>
+            <div className="overlay">
+                <Container>
+                    <div className="hero-content">
+                        <h1>Grow Your Professional</h1>
+                        <h1>Association Membership</h1>
+                        <Button size="lg">
+                            Attend a Webinar to Learn More!
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
